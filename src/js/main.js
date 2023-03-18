bedd6c735891e2730b06c4e449f6aabab3fa127d
Dominik <dstefanowski@gmail.com>
2023-03-18 09:33:12 +0100

    site update 1

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+// const test = () => {
+// 	console.log('test')
+// 	console.log('drugi test')
+// 	// koment
+// 	console.log(test)
+// }
+
+// test()
+
+const nav = document.querySelector('.nav')
+const burgerBtn = document.querySelector('.burger-btn')
+const navItems = document.querySelectorAll('.nav__item')
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active')
+	navItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+
+}
+
+
+
+burgerBtn.addEventListener('click', handleNav)
+
